e1496b941102f20eddad7c6c4d1552d70223c434
Md Shahzad Ali <shahzadalideveloper@outlook.com>
2024-04-06 13:18:37 +0530

    added new code changes

--- StarPattern/Program.cs
+++ StarPattern/Program.cs
@@ -7,6 +7,8 @@
             //Console.WriteLine("Enter the value of n");
 
             //int n = Convert.ToInt32(Console.ReadLine());
+
+            //Here n represents number of rows.
             int n = 5;
 
             //Right Half pyramid
@@ -64,7 +66,41 @@
                 {
                     Console.Write("* ");
                 }
-                Console.WriteLine() ;
+                Console.WriteLine();
+            }
+            Console.WriteLine("--------------------Full Pyramid Pattern--------------------------");
+
+            n = 10;
+            for (int i = 0; i <= n; i++)
+            {
+                // loop for printing leading whitespaces 
+                for (int j = 0; j < 2 * (n - i) - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < 2 * i + 1; k++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("---------------------Inverted full Pyramid Pattern-------------------------");
+            // first loop for printing all rows 
+            for (int i = 0; i < n; i++)
+            {
+
+                // first inner loop for printing white spaces 
+                for (int j = 0; j < 2 * i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                // second inner loop for printing star * 
+                for (int k = 0; k < 2 * (n - i) - 1; k++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
             }
         }
         static void Main(string[] args)
