48f203c5bc0cf114d76ea830398239604e947f10
shahz <shahz@CODEWITHSHAHZAD>
2024-04-06 12:55:07 +0530

    Add project files.

---
+++ StarPattern/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace StarPattern
+{
+    internal class Program
+    {
+        private void PyramidPattern()
+        {
+            //Console.WriteLine("Enter the value of n");
+
+            //int n = Convert.ToInt32(Console.ReadLine());
+            int n = 5;
+
+            //Right Half pyramid
+            Console.WriteLine("------------------Right Half Pyramid Pattern----------------------------");
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("---------------------Inverted Right Half Pyramid Pattern-------------------------");
+
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = n; j >= i; j--)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("--------------------Left Half Pyramid Pattern--------------------------");
+
+            n = 10;
+            for (int i = 0; i <= n; i++)
+            {
+                // loop for printing leading whitespaces 
+                for (int j = 0; j < 2 * (n - i) - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k <= i; k++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("---------------------Inverted Left Half Pyramid Pattern-------------------------");
+            // first loop for printing all rows 
+            for (int i = 0; i < n; i++)
+            {
+
+                // first inner loop for printing white spaces 
+                for (int j = 0; j < 2 * i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                // second inner loop for printing star * 
+                for (int k = 0; k < n - i; k++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine() ;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.PyramidPattern();
+            Console.ReadLine();
+        }
+    }
+}
